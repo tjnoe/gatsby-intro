6b8e724f21b099232a005ee613f22058bbeb8c2a
tjnoe <tjnoe01@gmail.com>
2020-01-08 23:16:07 -0500

    Set base styles with emotion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
-export default () => <div>Hi Frontend Masters!</div>;
+export default () => (
+  <Layout>
+    <h1>Home</h1>
+    <p>Hello Indianapolis!</p>
+    <Link to={`/about/`}>Learn about me &rarr;</Link>
+  </Layout>
+)
