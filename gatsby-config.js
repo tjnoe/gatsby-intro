74e1dd2e8081c6285a2bf692ba68db6ac8fa4c44
tjnoe <tjnoe01@gmail.com>
2020-01-14 22:01:06 -0500

    catch up

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,19 +7,36 @@ module.exports = {
   plugins: [
     `gatsby-plugin-emotion`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
         defaultLayouts: {
           default: require.resolve(`./src/components/layout.js`),
         },
+        gatsbyRemarkPlugins: [{ resolve: `gatsby-remark-images` }],
+        plugins: [{ resolve: `gatsby-remark-images` }],
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `posts`,
+        path: `${__dirname}/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-instagram`,
+      options: {
+        username: `gatsbyjs`,
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
+import Hero from '../components/hero'
 import PostPreview from '../components/post-preview'
+import Insta from '../components/insta'
 import usePosts from '../hooks/use-posts'
 
 export default () => {
   const posts = usePosts()
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Indianapolis!</p>
-      <Link to={`/about/`}>Learn about me &rarr;</Link>
-
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+        <Insta />
+      </Layout>
+    </>
   )
 }
